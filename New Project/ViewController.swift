6b32c4fff1c06a6830f4e1509fa1f00cedbc5818
Russell <rshtyrev@gmail.com>
2024-01-20 21:53:03 +0300

    bank account balance changed via GCD (dispatch work items)

--- New Project/ViewController.swift
+++ New Project/ViewController.swift
@@ -7,64 +7,85 @@
 
 import UIKit
 
-class BankAccount {
-    
-    var balance = 0
-    
-    func deposit () {
-        balance += 20
-    }
-    
-    func withdrawal () {
-        balance -= 10
-    }
-    
-    
-}
+//class BankAccount {
+//    
+//    var balance = 0
+//    
+//    func deposit () {
+//        balance += 20
+//    }
+//    
+//    func withdrawal () {
+//        balance -= 10
+//    }
+//    
+//}
 
 class ViewController: UIViewController {
     
-    let bankAccount = BankAccount()
-    
-    
+//    let bankAccount = BankAccount()
+//    
+//    
     let label = UILabel()
     let balanceLabel = UILabel()
-    
-    let lock = NSLock()
+//    
+//    let lock = NSLock()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        var balance = 0
         
-        let depositThread = Thread {
-            deposit()
-            return deposit()
-            }
-            depositThread.start()
+        let concurrentQueue = DispatchQueue(label: "Bank Account Balance", attributes: .concurrent)
+//        concurrentQueue.async {
+//            balance += 20
+//        }
+//        concurrentQueue.async {
+//            balance -= 10
+//        }
         
         
-        
-        let withdrawalThread = Thread {
-            withdrawal()
-            return withdrawal()
+        let depositItem = DispatchWorkItem {
+            balance += 20
         }
-            withdrawalThread.start()
-        
-        
-        
-        func deposit() {
-            lock.lock()
-            bankAccount.deposit()
-            lock.unlock()
+        depositItem.notify(queue: .main) {
+            print ("\(balance)$")
+        }
+        let withdrawalItem = DispatchWorkItem {
+            balance -= 10
         }
         
-        func withdrawal() {
-            lock.lock()
-            bankAccount.withdrawal()
-            lock.unlock()
-        }
-
-        
+        concurrentQueue.async(execute: depositItem)
+        concurrentQueue.async(execute: withdrawalItem)
+//        let depositThread = Thread {
+//            deposit()
+//            return deposit()
+//            }
+//            depositThread.start()
+//        
+//        
+//        
+//        let withdrawalThread = Thread {
+//            withdrawal()
+//            return withdrawal()
+//        }
+//            withdrawalThread.start()
+//        
+//        
+//        
+//        func deposit() {
+//            lock.lock()
+//            bankAccount.deposit()
+//            lock.unlock()
+//        }
+//        
+//        func withdrawal() {
+//            lock.lock()
+//            bankAccount.withdrawal()
+//            lock.unlock()
+//        }
+//
+//        
         self.view.addSubview(label)
         label.text = "Current Balance:"
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -72,10 +93,11 @@ class ViewController: UIViewController {
         label.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: -22).isActive = true
         
         self.view.addSubview(balanceLabel)
-        balanceLabel.text = "\(bankAccount.balance)$"
+        balanceLabel.text = "\(balance)$"
         balanceLabel.translatesAutoresizingMaskIntoConstraints = false
         balanceLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
         balanceLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        
         
         
         
